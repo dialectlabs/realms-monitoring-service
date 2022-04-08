c6dbe46e7714b3eb499a8cc004f98736e907b412
0xavro <0xavroarrow@gmail.com>
2022-04-08 09:53:16 -0400

    Add simple script to pull realms data

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,74 +1,28 @@
+import { getRealms, getAllProposals } from '@solana/spl-governance';
 import { Connection, PublicKey } from '@solana/web3.js';
-import { getMultipleSquadAccounts } from '../src/api/parseSquad';
+import { connect } from 'http2';
 
 async function run() {
-  const mainnetStagingPk = new PublicKey(
-    'og295qHEFgcX6WyaMLKQPwDMdMxuHoXe7oQ7ywwyRMo',
-  );
   const PROVIDER_URL =
-    'https://solana-api.syndica.io/access-token/6sW38nSZ1Qm4WVRN4Vnbjb9EF2QudlpGZBToMtPyqoXqkIenDwJ5FVK1HdWSqqah/rpc';
-  let connection = new Connection(PROVIDER_URL);
+  'https://solana-api.syndica.io/access-token/6sW38nSZ1Qm4WVRN4Vnbjb9EF2QudlpGZBToMtPyqoXqkIenDwJ5FVK1HdWSqqah/rpc';
+  const connection = new Connection(PROVIDER_URL);
+  const programId = new PublicKey('GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw');
 
-  console.log(
-    `\nGetting all program accounts for programId: ${mainnetStagingPk}.`,
-  );
-  let programAccounts = await connection.getProgramAccounts(mainnetStagingPk);
+  const realms = await getRealms(connection, programId);
 
-  // votes
-  // const voteAccountthreshold = 200;
-  // let voteAccounts = programAccounts.filter(
-  //   (it) => it.account.data.length < voteAccountthreshold,
-  // );
-  // console.log(`Total vote accounts: ${voteAccounts.length}`);
+  for (const realm of realms) {
+    console.log("name: ", realm.account.name);
+    console.log("accountType: ", realm.account.accountType);
+    console.log("votingProposalCount: ", realm.account.votingProposalCount);
+    console.log("realm all: ", realm);
 
-  // proposals
-  // let proposals = programAccounts.filter(
-  //   (it) => it.account.data.length < 8000 && it.account.data.length > 1000,
-  // );
-  // console.log(`Total proposal accounts: ${proposals.length}`);
-  // let propDeser = await getMultipleProposalAccounts(connection, proposals.slice(0, 2).map((it: any) => it.pubkey));
-  // console.log(propDeser.length)
-  // console.log(propDeser[0])
+    const proposals = await getAllProposals(connection, programId, realm.pubkey);
 
-  // squads
-  let squadsAccounts = programAccounts.filter(
-    (it) => it.account.data.length > 8000,
-  );
-  console.log(`Total squads accounts: ${squadsAccounts.length}`);
+    console.log("proposals", proposals);
 
-  let deserializedSquads = await getMultipleSquadAccounts(
-    connection,
-    squadsAccounts.map((it) => it.pubkey).slice(),
-  );
+    break;
+  }
 
-  console.log(`\nDeserialized ${deserializedSquads.length} squads...\n`);
-  deserializedSquads.map((squad) => {
-    console.log(
-      `\nChecking for members of squad ${squad.pubkey?.toBase58()}... Found ${
-        squad.members?.length
-      } members:\n`,
-    );
-    squad.members?.map((member) => console.log('  Member', member));
-  });
-
-  // old code
-  // let fst = squads[0];
-  // let members = fst.members;
-  // console.log(members)
-  // console.log(JSON.stringify(fst))
-  // console.log(squads.length)
 }
 
 run();
-
-enum ProposalType {
-  Text = 0,
-  Support = 1,
-  Quorum = 2,
-  WithdrawSol = 3,
-  WithdrawSpl = 4,
-  AddMember = 5,
-  RemoveMember = 6,
-  MintMemberToken = 7,
-  Swap = 8,
-}
