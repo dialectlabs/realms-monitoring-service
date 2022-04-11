2f30dde51e9f9830fea9e6f1da18d6d527e6c582
Alexey Tsymbal <alexey@gotiggy.com>
2022-04-12 02:15:30 +0400

    Remove RPC url from example

--- scripts/example.ts
+++ scripts/example.ts
@@ -6,9 +6,7 @@ import {
 import { Connection, PublicKey } from '@solana/web3.js';
 
 async function run() {
-  const PROVIDER_URL =
-    'https://solana-api.syndica.io/access-token/6sW38nSZ1Qm4WVRN4Vnbjb9EF2QudlpGZBToMtPyqoXqkIenDwJ5FVK1HdWSqqah/rpc';
-  const connection = new Connection(PROVIDER_URL);
+  const connection = new Connection(process.env.RPC_URL!);
   const programId = new PublicKey(
     'GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw',
   );
