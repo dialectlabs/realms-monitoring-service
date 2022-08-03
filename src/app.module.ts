26d72564abc054a1f6e6a69ba8aad4d11b88fd3d
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-08-03 15:21:19 +0400

    Configure logging and dialect sdk

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MonitoringServiceModule } from './monitoring-service/monitoring-service.module';
-import { ConfigModule } from '@nestjs/config';
-
-@Module({
-  imports: [MonitoringServiceModule, ConfigModule.forRoot()],
-  providers: [],
-})
-export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MonitoringModule } from './monitoring.module';
+import { Logger } from 'nestjs-pino';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create(MonitoringModule, {
     logger: ['log', 'warn', 'error'],
   });
-
-  await app.listen(process.env.PORT || 8080);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  const logger = app.get(Logger);
+  app.useLogger(logger);
+  console.trace = (...it) => logger.verbose(it);
+  console.debug = (...it) => logger.debug(it);
+  console.log = (...it) => logger.log(it);
+  console.info = (...it) => logger.log(it);
+  console.warn = (...it) => logger.warn(it);
+  console.error = (...it) => logger.error(it);
+  await app.listen(process.env.PORT ?? 0);
 }
 
 bootstrap();
